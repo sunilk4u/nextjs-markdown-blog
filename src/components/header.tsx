4e153c1b0e5c419d217870c3c7b709b0a8309a2a
sunilk4u <sunilk10099@gmail.com>
2023-05-02 16:01:55 +0530

    remove div

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,12 @@ const Header = () => {
     <header>
       <div>
         <div>
-          <h1 itemProp="headline">
-            <a href="https://thecuriouscorp.com/">{process.env.HomePage_Title}</a>
+          <h1>
+            <a href="https://thecuriouscorp.com/">
+              {process.env.HomePage_Title}
+            </a>
           </h1>
         </div>
-        <div></div>
       </div>
     </header>
   );
