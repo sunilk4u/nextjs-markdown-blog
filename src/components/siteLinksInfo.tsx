ae18c48620537a7ea49b55c933a1c688ead5e9e7
sunilk4u <sunilk10099@gmail.com>
2023-05-02 18:14:34 +0530

    add sitelinks section content

--- src/components/siteLinksInfo.tsx
+++ src/components/siteLinksInfo.tsx
@@ -1,6 +1,31 @@
 const SiteLinksInfo = () => {
   return (
-    <div>SiteLinksInfo</div>
+    
+    <div>
+      <div>
+        <h4>Important Links</h4>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Disclaimer</li>
+          <li>Privacy Policy</li>
+          <li>DCMA</li>
+          <li>Contact us</li>
+        </ul>
+      </div>
+      <div>
+        <h4>About Us</h4>
+        <p>This is a tech blog. Built using next.js and markdown posts as jamstack blog. You can write amazing articles using this template.</p>
+      </div>
+      <div>
+        <h4>Follow Us</h4>
+        <ul>
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>Twitter</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
