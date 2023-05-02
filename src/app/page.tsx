fc9d136062621566cc1e5b776c0866d0145d1187
sunilk4u <sunilk10099@gmail.com>
2023-05-02 16:01:23 +0530

    add list of posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import fs from "fs";
+
+const getPostMetaData = () => {
+  const files = fs.readdirSync("src/posts");
+  const mdPosts = files.filter((file) => file.endsWith(".md"));
+  const slugs = mdPosts.map((post) => post.replace(".md", ""));
+
+  return slugs;
+};
+
 const HomePage = () => {
+  const data = getPostMetaData();
   return (
-    <div><p className="text-display">HomePage</p></div> 
-  )
-}
+    <div>
+      {data.map((post) => (
+        <h2 key={post}>{post}</h2>
+      ))}
+    </div>
+  );
+};
 
 export default HomePage;
