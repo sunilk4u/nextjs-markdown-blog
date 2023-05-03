491a053d362f36860a4446c2ffde1b58f0e0238f
sunilk4u <sunilk10099@gmail.com>
2023-05-03 16:52:55 +0530

    add sitelinks styles

--- src/components/siteLinksInfo.tsx
+++ src/components/siteLinksInfo.tsx
@@ -1,32 +1,38 @@
+import "../styles/sitelinks.css"
+
 const SiteLinksInfo = () => {
   return (
-    
-    <div>
-      <div>
-        <h4>Important Links</h4>
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Disclaimer</li>
-          <li>Privacy Policy</li>
-          <li>DCMA</li>
-          <li>Contact us</li>
-        </ul>
-      </div>
-      <div>
-        <h4>About Us</h4>
-        <p>This is a tech blog. Built using next.js and markdown posts as jamstack blog. You can write amazing articles using this template.</p>
-      </div>
-      <div>
-        <h4>Follow Us</h4>
-        <ul>
-          <li>Facebook</li>
-          <li>Instagram</li>
-          <li>Twitter</li>
-        </ul>
+    <div className="bg-green-800">
+      <div className="container mx-auto md:flex md:justify-between">
+        <div className="md:flex-col m-4">
+          <h4 className="text-white text-lg font-medium">Important Links</h4>
+          <ul className="grid grid-cols-1 gap-3 m-2 sitelinks">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Disclaimer</li>
+            <li>Privacy Policy</li>
+            <li>DCMA</li>
+            <li>Contact us</li>
+          </ul>
+        </div>
+        <div className="md:flex-col m-4 md:w-[30%] text-white">
+          <h4 className="text-white text-lg font-medium">About Us</h4>
+          <p className="m-2">
+            This is a tech blog. Built using next.js and markdown posts as
+            jamstack blog. You can write amazing articles using this template.
+          </p>
+        </div>
+        <div className="md:flex-col m-4">
+          <h4 className="text-white text-lg font-medium">Follow Us</h4>
+          <ul className="grid grid-cols-1 gap-3 m-2 sitelinks">
+            <li>Facebook</li>
+            <li>Instagram</li>
+            <li>Twitter</li>
+          </ul>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SiteLinksInfo
+export default SiteLinksInfo;
