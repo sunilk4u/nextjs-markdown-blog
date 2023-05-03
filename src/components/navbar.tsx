7d71a2badb2a21e0d3fb018d5dfba3dd6a032cc1
sunilk4u <sunilk10099@gmail.com>
2023-05-03 12:27:45 +0530

    add navbar css

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,16 +5,21 @@ const NavBar = () => {
   const categoryData = getAllCategories();
 
   return (
-    <nav>
-      <ul>
-        {categoryData.map((category: string) => (
-          <li key={category}>
-            <Link href={`category/${category.replaceAll(" ", "-")}`}>
-              {category}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <nav className="bg-green-800">
+      <div className="container mx-auto">
+        <ul className="flex items-center h-14">
+          {categoryData.map((category: string) => (
+            <li
+              key={category}
+              className="flex items-center h-full text-white text-lg px-5 uppercase hover:bg-green-600 transition ease-in-out"
+            >
+              <Link href={`category/${category.replaceAll(" ", "-")}`}>
+                {category.replace("-", " ")}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
