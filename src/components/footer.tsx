a70d108a4a45e4955b4976110f666631ef2bb21c
sunilk4u <sunilk10099@gmail.com>
2023-05-03 16:21:36 +0530

    add footer styles

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,12 +2,13 @@ const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer>
-      <footer>
+    <footer className="bg-green-950 h-14 flex items-center justify-center">
         <div>
-          <p>Copyright ©&nbsp;{year} {process.env.blogUrl} | built by <a href="https://github.com/sunilk4u">Sunil Kumar</a></p>
+          <p className="text-white text-lg">
+            Copyright ©&nbsp;{year} {process.env.blogUrl} | built by{" "}
+            <a className="hover:text-green-400" href="https://github.com/sunilk4u">Sunil Kumar</a>
+          </p>
         </div>
-      </footer>
     </footer>
   );
 };
